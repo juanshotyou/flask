f6c2c4fa3c574cbbeb3b276bfe508f4c606dfd1d
Vlad R <vlad.raducanu@outlook.com>
2021-10-01 19:45:30 +0100

    Added user log in functions

--- toolkit/app/forms.py
+++ toolkit/app/forms.py
@@ -5,5 +5,5 @@ from wtforms.validators import DataRequired
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
+    remember_me = BooleanField('Remember me')
     submit = SubmitField('Sign In')
