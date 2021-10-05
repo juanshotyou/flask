08a7caa6e86237103c0df21f5420b66efc479ba0
Vlad R <vlad.raducanu@outlook.com>
2021-10-05 18:04:05 +0100

    Reached chapter 11 from the Flask tutorial - added user registration + small changes

--- toolkit/app/forms.py
+++ toolkit/app/forms.py
@@ -1,9 +1,31 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, DataRequired, Email, EqualTo, Length
+from app.models import User
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember me')
-    submit = SubmitField('Sign In')
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField("Remember me")
+    submit = SubmitField("Sign In")
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    password2 = PasswordField("Repeat password", validators=[DataRequired(), EqualTo("password")])
+    submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError("Username is already taken. Please choose a different username.")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError("Email address is already taken. Please choose a different email address.")
+
+class PostForm(FlaskForm):
+    post = TextAreaField("Say something", validators = [DataRequired(), Length(min=1, max=1400)])
+    submit = SubmitField("Submit")
--- toolkit/app/routes.py
+++ toolkit/app/routes.py
@@ -1,16 +1,24 @@
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm, PostForm
+from app.models import Post
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
 
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
 @login_required
 def index():
-    return render_template("index.html", title="Home")
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash("Post successfull!")
+        return redirect(url_for("index"))
+    return render_template("index.html", title="Home", form=form)
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/login", methods = ["GET", "POST"])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for("index"))
@@ -23,26 +31,36 @@ def login():
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get("next")
         if not next_page or url_parse(next_page).netloc != "":
-            next_page = url_for('index')
+            next_page = url_for("index")
         return redirect(next_page)
     return render_template("login.html", title="Sign In", form=form)
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
+
+@app.route("/register", methods = ["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Registration successfull!")
+        return redirect(url_for("login"))
+    return render_template("register.html", title="Register", form=form)
+
+@app.route("/user/<username>")
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template("user.html", user=user)
 
 @app.route("/firepower")
 @login_required
 def firepower():
     return render_template("index.html", title="Firepower")
-
-@app.route("/webex")
-@login_required
-def webex():
-    return render_template("index.html", title="Webex")
-
-@app.route("/meraki")
-@login_required
-def meraki():
-    return render_template("index.html", title="Meraki")
